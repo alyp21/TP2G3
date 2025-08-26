41622ed3f1e5e63fb35424fb7c99fa85bfeac4ba
alyy <alybelp7@gmail.com>
2025-08-25 22:12:24 -0300

    Update ClasePrueba.java

--- src/ClasePrueba.java
+++ src/ClasePrueba.java
@@ -2,9 +2,9 @@
 public class ClasePrueba {
     
     // holis soy aly !!!
-<<<<<<< HEAD
+    
     //hago otra prueba a ver si funciona 
-=======
+    
     //Probando probando soy cristian :D
->>>>>>> 31b254632d72620175de23e6f312bea420c41b3d
+    
 }
