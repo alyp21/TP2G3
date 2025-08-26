a2cf3680eb735368a4ff356b2768b2b33767ee1b
Abstra09 <fede-palma@hotmail.com>
2025-08-25 23:24:56 -0300

    Pequeños ajustes, grandes intenciones

--- src/JavaPackage/ClasePrueba2.java
+++ src/JavaPackage/ClasePrueba2.java
@@ -2,5 +2,10 @@
 package JavaPackage;
 
 public class ClasePrueba2 {
+    private String apellido;
+    private int edad;
     
+    public void cantar(){
+        System.out.println("Y si hacemos un muñeco");
+    }
 }
