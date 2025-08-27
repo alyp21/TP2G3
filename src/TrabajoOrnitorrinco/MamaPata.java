925cb1ec47f99add8509c5749a0b8c54bce03a97
kevinorozcocarp93 <kevinorozcocarp@gmail.com>
2025-08-27 20:55:12 -0300

    mama pata

--- src/TrabajoOrnitorrinco/MamaPata.java
+++ src/TrabajoOrnitorrinco/MamaPata.java
@@ -2,5 +2,8 @@
 package TrabajoOrnitorrinco;
 
 public interface MamaPata {
+    public final double PICO = 6.8;
+    public final double PATAS= 7.5;
     
+    public abstract void tocarOrgano ();
 }
