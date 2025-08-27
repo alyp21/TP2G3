60220e56a880e957454a80bfcf740bb86fdc2f82
Kuristianx <cristianisaiasaccone1406@gmail.com>
2025-08-27 20:42:18 -0300

    Complete la clase castor

--- src/TrabajoOrnitorrinco/Castor.java
+++ src/TrabajoOrnitorrinco/Castor.java
@@ -1,6 +1,24 @@
 
 package TrabajoOrnitorrinco;
 
+import java.util.Random;
+
 public class Castor {
+    protected double cola;
+    protected double velocidad;
+
+    public Castor(double cola, double velocidad) {
+        this.cola = cola;
+        this.velocidad = velocidad;
+    }
     
+    public void tocarGuitarra(){
+        Random numRandom=new Random();
+        int numero=numRandom.nextInt(6)+1;
+        System.out.println("Toca cuerda"+numero);
+    }
+    
+    public void nadar(){
+        System.out.println("Nada a la velocidad de "+velocidad+"km/h");
+    }
 }
