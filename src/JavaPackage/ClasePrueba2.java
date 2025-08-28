f3504e7e7e301b93373832a63747560e3265074c
Abstra09 <fede-palma@hotmail.com>
2025-08-27 23:32:21 -0300

    OrniVerde les dejo por si quieren modificar algo

--- src/JavaPackage/ClasePrueba2.java
+++ src/JavaPackage/ClasePrueba2.java
@@ -6,6 +6,6 @@ public class ClasePrueba2 {
     private int edad;
     
     public void cantar(){
-        System.out.println("Y si hacemos un muñeco");
+        System.out.println("Y si hacemos un muñeco");       
     }
 }
