5780e0a379991b8c8c766c71f59d3a19933561c3
kevinorozcocarp93 <kevinorozcocarp@gmail.com>
2025-08-27 23:20:43 -0300

    Orni azullll

--- src/TrabajoOrnitorrinco/OrnitorrincoBlue.java
+++ src/TrabajoOrnitorrinco/OrnitorrincoBlue.java
@@ -1,6 +1,31 @@
 
 package TrabajoOrnitorrinco;
 
-public class OrnitorrincoBlue {
+public class OrnitorrincoBlue extends Castor{
+    private String nombre;
+    private double propulsion;
+
+    public OrnitorrincoBlue(String nombre, double propulsion, double cola, double velocidad) {
+        super(cola, velocidad);
+        this.nombre = nombre;
+        this.propulsion = propulsion;
+    }
+
+    public double getPropulsion() {
+        return propulsion;
+    }
+
+    @Override
+    public void nadar() {
+        double velocidadFinal = velocidad + propulsion; 
+        System.out.println(nombre + "nada con velocidad " + velocidadFinal+ "km/h");
+    }
+    @Override
+    public void tocarGuitarra(){
+        System.out.println(nombre + "toca la guitarra");
+        super.tocarGuitarra();
+    }
+
+    
     
 }
