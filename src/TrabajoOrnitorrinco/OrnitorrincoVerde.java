17d088b3eab157ea07d590e71fa4ab71cb8c6297
Abstra09 <fede-palma@hotmail.com>
2025-08-27 23:54:25 -0300

    Realizacion de subclase OrnitorrincoVerde

--- src/TrabajoOrnitorrinco/OrnitorrincoVerde.java
+++ src/TrabajoOrnitorrinco/OrnitorrincoVerde.java
@@ -1,6 +1,33 @@
 
 package TrabajoOrnitorrinco;
 
-public class OrnitorrincoVerde {
-    
+import java.util.Scanner;
+
+public class OrnitorrincoVerde extends Castor implements MamaPata {
+    private final double PICO = 6.8;
+    private final double PATAS = 7.5;
+    private String nombre;
+
+    public OrnitorrincoVerde(String nombre, double velocidad, double cola) {
+        super(velocidad, cola);
+        this.nombre = nombre;
+    }
+
+   @Override
+   public void tocarOrgano (){
+   Scanner scanner = new Scanner(System.in);
+       System.out.println("Do-Re-Mi (Presiona una tecla para continuar)");
+       scanner.nextLine();
+       System.out.println("Fa-Sol-La-Si");
 }
+   public void tocarGuitorgan(){
+   super.tocarGuitarra();
+   tocarOrgano();
+       System.out.println(" cuac...cuaac...!");
+ 
+   }
+   @Override 
+   public void tocarGuitarra(){
+   super.tocarGuitarra();
+   }
+ }
